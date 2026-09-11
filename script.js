3a97f438f08e4f0577ea7a20f1401a68c9f7f551
bsarika2366-pixel <b.sarika2366@gmail.com>
2026-09-11 14:33:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const button =
+document.querySelector("button");
+ button.addEventListener("click",function() {
+     alert(
+           "nv shreya gurthu osthundi ante bayam ithundi ekkada maripothavo ani\u{1F97A}\n\ni love you so muchhhhhhh nanna\u{2764}\u{FE0F} \u{1FAC2} \n\n nv natho matladaka pothe chala bada esthadi ra\n\n nanu patinchukovatledu bujji nv asalu chala edupu osthundi \u{1F614}.");
+     });
+ 
